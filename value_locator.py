591c3983eba5edb65b249b7a9c873c3b2edbb0fd
Jomark <maguadjomarkglenn@gmail.com>
2024-10-16 12:14:00 +0800

    pseudocode

---
+++ value_locator.py
@@ -0,0 +1,8 @@
+#Define 5 variables
+#Compare variables 1 to 2
+#If variable 1 greater than 2
+    #Compare variable 1 to 3
+    #If variable 1 greater than 3
+    #Return to Variable 1, else return to variable 3
+#Repeat process until all 5 variables have been compared
+#Ask for user input and find highest variable
