b5474311ba2290f8244a968f9c71c357357041ea
Jomark <maguadjomarkglenn@gmail.com>
2024-10-16 22:13:21 +0800

    some_fix

--- value_locator.py
+++ value_locator.py
@@ -1,3 +1,11 @@
+#Define 5 variables
+#Compare variables 1 to 2
+#If variable 1 greater than 2
+    #Compare variable 1 to 3
+    #If variable 1 greater than 3
+    #Return to Variable 1, else return to variable 3
+#Repeat process until all 5 variables have been compared
+#Ask for user input and find highest variable
 def find_highest(first, second, third, fourth, fifth):
     highest = first
 
@@ -14,6 +22,5 @@ def find_highest(first, second, third, fourth, fifth):
         highest = fifth
 
 print("The highest number is: ", highest)
-
 
 
